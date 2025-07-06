0452a043e01794a8c1a190d627c93ab2f99f2998
Petar Šimeta <petar.simeta@perpetuum.hr>
2025-07-06 11:35:22 +0200

    returned images to 1200, added open graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,18 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Marija i Petar",
-  description: "Slike sa svadbe",
+  description: "Slike s naše svadbe",
+  openGraph: {
+    title: "Marija i Petar",
+    description: "Slike s naše svadbe",
+    type: "website",
+    siteName: "Marija i Petar",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dnmrg6knf/image/upload/c_fit,w_1200,q_auto,f_auto/153_bvqltp",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
